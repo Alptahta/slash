55d89aca6bddc2d6f759e923071c5b9a7d0d1c57
Alp Tahta <alp.tahta@protonmail.com>
2023-05-19 01:51:17 +0300

    init user handler and unit test

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const HTTP_SERVER_PORT = "8080"
 
 func main() {
-	fmt.Println("Init main")
+	log.Printf("Starting HTTP server at port %s", HTTP_SERVER_PORT)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/users", createUser)
+
+	portString := fmt.Sprintf(":%s", HTTP_SERVER_PORT)
+	log.Fatal(http.ListenAndServe(portString, mux))
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	resp := "TODO create user"
+	w.Write([]byte(resp))
 }
