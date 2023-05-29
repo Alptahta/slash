222b092caada83478417f07052dd92bdfac105b7
Alptahta <alp.tahta@protonmail.com>
2023-05-29 21:54:55 +0300

    Refactor Server Config

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,12 +8,16 @@ import (
 const HTTP_SERVER_PORT = "8080"
 
 func main() {
-	log.Printf("Starting HTTP server at port %s", HTTP_SERVER_PORT)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/users", createUser)
 
-	portString := fmt.Sprintf(":%s", HTTP_SERVER_PORT)
-	log.Fatal(http.ListenAndServe(portString, mux))
+	srv := &http.Server{
+		Addr:    HTTP_SERVER_PORT,
+		Handler: mux,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+	log.Printf("Starting HTTP server at port %s", HTTP_SERVER_PORT)
+	mux.HandleFunc("/users", createUser)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
