ec8dcfc124b2dae6c7a717f32ad78d93d13e7b76
Alptahta <alp.tahta@protonmail.com>
2023-05-29 23:17:26 +0300

    Add unit test for createUserHandler method

---
+++ router/restHandlers_test.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_createUser(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/users", nil)
+	w := httptest.NewRecorder()
+	uh := NewUserHandler()
+	uh.ServeHTTP(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "TODO create user" {
+		t.Errorf("expected TODO create user got %v", string(data))
+	}
+}
