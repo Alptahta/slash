faa168f1fd1bb6e9ba8539d72134cc0c5fc95129
Alptahta <alp.tahta@protonmail.com>
2023-05-30 17:08:35 +0300

    Add unit test for not found logic

--- router/restHandlers_test.go
+++ router/restHandlers_test.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -20,5 +21,22 @@ func Test_createUserHandler(t *testing.T) {
 	}
 	if string(data) != "TODO create user" {
 		t.Errorf("expected TODO create user got %v", string(data))
+	}
+}
+
+func Test_NotFound(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	w := httptest.NewRecorder()
+	uh := NewUserHandler()
+	uh.ServeHTTP(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	fmt.Println("a" + string(data) + "a")
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "404 page not found\n" {
+		t.Errorf("expected 404 page not found got %v", string(data))
 	}
 }
