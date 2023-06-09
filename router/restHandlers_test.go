fe77bd7cd5a99b6c508fcf8bea83221e1880a954
Alptahta <alp.tahta@protonmail.com>
2023-06-10 02:23:08 +0300

    Improve unit test readablity and exit on errors logic

--- router/restHandlers_test.go
+++ router/restHandlers_test.go
@@ -8,33 +8,43 @@ import (
 )
 
 func Test_createUserHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodPost, "/users", nil)
-	w := httptest.NewRecorder()
-	uh := NewUserHandler()
-	uh.ServeHTTP(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-	if string(data) != "TODO create user" {
-		t.Errorf("expected TODO create user got %v", string(data))
-	}
+	t.Run("Should return successful response with HTTP status:201", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/users", nil)
+		w := httptest.NewRecorder()
+		uh := NewUserHandler()
+		uh.ServeHTTP(w, req)
+		res := w.Result()
+		defer res.Body.Close()
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Fatalf("expected error to be nil got %v", err)
+		}
+		if string(data) != "TODO create user" {
+			t.Fatalf("expected TODO create user got %v", string(data))
+		}
+		if res.StatusCode != http.StatusCreated {
+			t.Fatalf("expected HTTP Status Code 200 got %v", res.StatusCode)
+		}
+	})
 }
 
 func Test_NotFound(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/users", nil)
-	w := httptest.NewRecorder()
-	uh := NewUserHandler()
-	uh.ServeHTTP(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-	if string(data) != "404 page not found\n" {
-		t.Errorf("expected 404 page not found got %v", string(data))
-	}
+	t.Run("Should return HTTP status:404 when POST method not used", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/users", nil)
+		w := httptest.NewRecorder()
+		uh := NewUserHandler()
+		uh.ServeHTTP(w, req)
+		res := w.Result()
+		defer res.Body.Close()
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Fatalf("expected error to be nil got %v", err)
+		}
+		if string(data) != "404 page not found\n" {
+			t.Fatalf("expected 404 page not found got %v", string(data))
+		}
+		if res.StatusCode != http.StatusNotFound {
+			t.Fatalf("expected HTTP Status Code 404 got %v", res.StatusCode)
+		}
+	})
 }
